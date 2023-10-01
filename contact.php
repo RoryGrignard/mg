5ef41e172db55899a5daf86ce27ac9c9a6795604
Rory Grignard <rorygrignard@protonmail.com>
2023-10-01 19:35:01 +0200

    Updated file structure

--- contact.php
+++ contact.php
@@ -1,5 +1,15 @@
+<?php 
+/*
+Template Name: Contact
+*/
+?>
+
 <?php get_header(); ?>
 
+<h1><?php the_title(); ?></h1>
+
 <?php get_template_part('includes/section', 'content'); ?>
+
+<h3>Contact info</h3>
 
 <?php get_footer(); ?>
