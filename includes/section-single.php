653d99501151f3f930f6fceb08ccf0b2ee830602
Rory Grignard <rorygrignard@protonmail.com>
2023-12-08 15:45:08 +0200

    Added tags to section-single

--- includes/section-single.php
+++ includes/section-single.php
@@ -7,8 +7,19 @@
 
     <?php
     $first_name = get_the_author_meta('first_name');
-     $last_name = get_the_author_meta('last_name');
+    $last_name = get_the_author_meta('last_name');
     ?>
 
     <h4>Author: <?php echo $first_name; ?> <?php echo $last_name; ?></h4>
+
+    <?php
+    $tags = get_the_tags();
+    if($tags):
+        foreach($tags as $tag): ?>
+            <a href="<?php echo get_tag_link($tag->term_id); ?>" class="single__tag">
+                <?php echo $tag->name; ?>
+            </a>
+        <?php endforeach; ?>
+    <?php endif; ?>
+
 <?php endwhile; endif; ?>
