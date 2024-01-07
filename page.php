63bb9394cb65f09fb8474265e699f193dcdc9d4b
Rory Grignard <rorygrignard@protonmail.com>
2024-01-07 15:50:28 +0200

    feat: Integrate Tailwind CSS into WordPress theme
    - Added Tailwind CSS, PostCSS, and Autoprefixer as dependencies
    - Configured PostCSS to process CSS with Tailwind CSS and Autoprefixer
    - Added 'build' script to compile CSS with PostCSS
    - Updated functions.php to enqueue the processed CSS file

--- page.php
+++ page.php
@@ -3,7 +3,7 @@
 <div class="container page">
     <?php if(has_post_thumbnail()): ?>
         <?php $thumbnail_id = get_post_thumbnail_id( $post->ID ); ?>
-        <img src="<?php the_post_thumbnail_url('post-thumbnail-large'); ?>" alt="<?php echo get_post_meta( $thumbnail_id, '_wp_attachment_image_alt', true ); ?>">
+        <img src="<?php the_post_thumbnail_url('post-thumbnail-large'); ?>" alt="<?php echo get_post_meta( $thumbnail_id, '_wp_attachment_image_alt', true ); ?>" class="w-full">
     <?php endif; ?>
 
     <h1><?php the_title(); ?></h1>
