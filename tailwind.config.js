0a232979d526354b3a6a89bd098d6bfee04014b8
Rory Grignard <rorygrignard@protonmail.com>
2024-01-14 09:44:08 +0200

    Updating existing CSS to use Tailwind utilities  (WIP)

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,23 @@ module.exports = {
     '**/*.php'
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        white: '#FFF',
+        tundora: '#4A4A4A',
+        royalPurple: '#6C32A6',
+        gigas: '#58328C',
+        cerulean: '#04B2D9',
+        persianGreen: '#03A6A6',
+        dawnPink: '#EFE7DC',
+        whiteLinen: '#F9F3EA'  
+      },
+      fontFamily: {
+        sans: ['Poppins', 'sans-serif'],
+        serif: ['Poppins', 'sans-serif'],
+        heading: ['"Playfair Display"', 'sans-serif'],
+      }
+    },
   },
   plugins: [],
 }
